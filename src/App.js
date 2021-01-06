e07a033b7a4cd274f1860bd85449e37fd97310a7
ForDzq <2187758898@qq.com>
2021-01-06 14:53:45 +0800

    按需引入antd,配置主题颜色,引入vue-router,reset.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import {HashRouter,Switch,Route} from 'react-router-dom'
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      app--------------
+      <HashRouter>
+        <Switch>
+          <Route path="/login" component={Login}/>
+          <Route path="/" component={Admin}/>
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
-
+import {Button,message} from 'antd'
 export default class Admin extends Component {
+  clickHandler = () => {
+    message.success('点击了按钮')
+  }
   render() {
     return (
       <div>
-        admin
+        admin-------
+        <Button type="primary" onClick={this.clickHandler}>按钮</Button>
       </div>
     )
   }
